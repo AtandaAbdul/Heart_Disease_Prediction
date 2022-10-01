0ba86c947fcf7f4065882268c30116d49fbe001d
AtandaAbdul <103516124+AtandaAbdul@users.noreply.github.com>
2022-10-01 05:23:29 +0100

    Update heart_prediction.py

--- heart_prediction.py
+++ heart_prediction.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle  #to load a saved model
 import base64  #to open .gif files in streamlit app
+from sklearn.preprocessing import StandardScaler
 import sklearn
 
 
